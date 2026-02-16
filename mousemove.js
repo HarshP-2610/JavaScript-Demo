fb0fb5370709713249e087f5caf5794a759f2c3d
HarshP-2610 <harsh261120@gmail.com>
2026-02-16 16:30:50 +0530

    MouseMove Chnages

--- mousemove.js
+++ mousemove.js
@@ -1,9 +1,8 @@
 const follower = document.getElementById('follower');
 
 window.addEventListener('mousemove', (e) => {
-
     const x = e.clientX;
+    // const x = e.clientX;
     const y = e.clientY;
-
     follower.style.transform = `translate(${x - 40}px, ${y - 40}px)`;
 });
